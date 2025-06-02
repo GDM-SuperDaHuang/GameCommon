ec72d32e814ec6884681099bdc3f9dba317fa1e6
gdm <1875525594@qq.com>
2025-06-02 17:06:13 +0800

    添加压缩，加密

---
+++ common/src/main/java/com/slg/module/connection/DHKeyInfo.java
@@ -0,0 +1,27 @@
+package com.slg.module.connection;
+import java.math.BigInteger;
+
+public class DHKeyInfo {
+    //测试消息
+    private BigInteger privateKey;  // 服务器私钥
+    private BigInteger publicKey;   // 服务器公钥
+    private BigInteger sharedKey;   // 共享密钥
+
+    public DHKeyInfo(BigInteger privateKey, BigInteger publicKey, BigInteger sharedKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+        this.sharedKey = sharedKey;
+    }
+
+    public BigInteger getPrivateKey() {
+        return privateKey;
+    }
+
+    public BigInteger getPublicKey() {
+        return publicKey;
+    }
+
+    public BigInteger getSharedKey() {
+        return sharedKey;
+    }
+}
